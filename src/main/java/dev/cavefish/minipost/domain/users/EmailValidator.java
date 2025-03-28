98a986a41eff037685b1172d0b8720f94f12d3f0
cavefish <cavefish@gmail.com>
2025-03-28 09:56:04 +0100

    Add example for email verification

--- src/main/java/dev/cavefish/minipost/domain/users/EmailValidator.java
+++ src/main/java/dev/cavefish/minipost/domain/users/EmailValidator.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailValidator {
+
+    // Validates following https://datatracker.ietf.org/doc/html/rfc5322#section-3.4.1
     public boolean isValid(String email) {
         // TODO implement this method using TDD
+        // https://en.wikipedia.org/wiki/Email_address#Examples
         return true;
     }
 }
